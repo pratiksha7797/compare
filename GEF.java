8038503a4ed4bbd851b1d549d555dd0548a932b3
pratiksha7797 <92936180+pratiksha7797@users.noreply.github.com>
2021-11-13 22:31:29 +0530

    Add files via upload

---
+++ GEF.java
@@ -0,0 +1,35 @@
+// Java Program to Compare Elements in a Collection
+import java.io.*;
+import java.util.*;
+
+class GEF {
+    public static void main(String[] args)
+    {
+        // List initialization
+        List<Integer> l = new ArrayList<>();
+
+        // Add elements in the list
+        l.add(3);
+        l.add(5);
+        l.add(18);
+        l.add(4);
+        l.add(6);
+
+        
+        int minimum = Collections.min(l);
+
+        // Maximum in the list
+        int maximum = Collections.max(l);
+
+        if (minimum == maximum) {
+            System.out.println("All elements are equal");
+        }
+        else {
+            System.out.println("Min value of our list : "
+                    + minimum);
+            System.out.println("Max value of our list : "
+                    + maximum);
+        }
+    }
+}
+
